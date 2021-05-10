745f2d3244caca750b9c5ca0d5bf91641b54e960
uchi0824 <riversidetown@icloud.com>
2021-05-10 18:49:32 +0900

    my first React Hook with useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const initialCount = 0;
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  const increment2 = () => setCount((previousCount) => previousCount + 1);
+  const decrement2 = () => setCount((previousCount) => previousCount - 1);
+
+  const reset = () => setCount(initialCount);
+
+  const double = () => setCount((previousCount) => previousCount * 2);
+
+  const divide3 = () =>
+    setCount((previousCount) =>
+      previousCount % 3 === 0 ? previousCount / 3 : previousCount
+    );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>reset</button>
+        <button onClick={double}>x2</button>
+        <button onClick={divide3}>３の倍数の時だけ３で割る</button>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
